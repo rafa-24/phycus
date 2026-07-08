e875f5d808aa0267ec9059a6b1e8461f1b97d97a
rafa-24 <rafaprieto063@gmail.com>
2026-07-08 18:30:28 -0500

    add: cors

--- app/main.py
+++ app/main.py
@@ -4,7 +4,25 @@ from app.modules.users.controller.user_controller import user
 from app.modules.auth.controller.auth_controller import auth
 from app.modules.shared.exceptions.exception_handlers import register_exception_handlers
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+# Defino las rutas que pueden enviar peticiones a phycus
+origins = [
+    "http://localhost.tiangolo.com",
+    "http://localhost:8080"
+]
+
+# metodos permitidos
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_exception_handlers(app)
 
@@ -17,6 +35,5 @@ async def root():
     return {"message": "Phycus api V.0.0.1"}
 
 # incluir rutas en mi aplicacion
-
 app.include_router(user)
 app.include_router(auth)
